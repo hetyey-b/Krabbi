525881af35302791411e49c0b256c2d2dc10f222
hetyey-b <hetyey.botond@gmail.com>
2023-03-10 12:16:07 +0100

    added Tile struct

--- src/game/board.rs
+++ src/game/board.rs
@@ -1,16 +1,29 @@
+#[derive(Clone, Copy)]
+pub enum Tile {
+    Empty,
+    Corner,
+    Black,
+    White,
+    King,
+    ThroneWithKing,
+    ThroneEmpty,
+}
+
 struct BoardState {
-    board: [[u8; 11]; 11],
+    board: [[Tile; 11]; 11],
 }
 
 impl BoardState {
     fn new() -> BoardState {
-        let mut new_board = [[0;11]; 11];
+        let mut new_board = [[Tile::Empty;11]; 11];
 
-        new_board[0][0] = 1;
-        new_board[0][11] = 1;
-        new_board[11][0] = 1;
-        new_board[11][11] = 1;
-        new_board[5][5] = 2;
+        // set the corner points
+        new_board[0][0] = Tile::Corner;
+        new_board[0][11] = Tile::Corner;
+        new_board[11][0] = Tile::Corner;
+        new_board[11][11] = Tile::Corner;
+        // set the throne
+        new_board[5][5] = Tile::ThroneEmpty;
 
         BoardState { 
             board: new_board,
@@ -21,9 +34,11 @@ impl BoardState {
         for i in 0..self.board.len() {
             for j in 0..self.board[i].len() {
                 match self.board[i][j] {
-                    0 => print!("0"),
-                    1 | 2 => print!("X"),
-                    _ => print!(" "),
+                    Tile::Empty => print!("0"),
+                    Tile::Corner | Tile::ThroneEmpty => print!("X"),
+                    Tile::Black => print!("B"),
+                    Tile::White => print!("W"),
+                    Tile::King | Tile::ThroneWithKing => print!("K"),
                 }
             }
             println!("");
