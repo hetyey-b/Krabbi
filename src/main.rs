95263ba8b3c6d38b6fb856ee3195685f85efe66a
hetyey-b <hetyey.botond@gmail.com>
2023-03-13 20:02:46 +0100

    Initial version of REST API web server

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
-use game::board::{Tile,Board,Passable};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 
 pub mod game;
 
-fn main() {
-    let mut board: Board = Board::new();
-    board.set_tile(Tile::White, 4, 5);
-    board.print_board();
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!\n")
+}
 
-    println!("");
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Loading .env variables...");
+    dotenv().ok(); // This line loads the environment variables
 
-    if board.get_tile(4, 0).unwrap().passable() {
-        println!("passable");
-    } else {
-        println!("not passable");
-    }
+    let web_server_ip: &str = &std::env::var("WEB_SERVER_IP").expect("WEB_SERVER_IP missing from .env");
+    let web_server_port: u16 = std::env::var("WEB_SERVER_PORT").expect("WEB_SERVER_PORT missing from .env").parse::<u16>().unwrap();
+
+    println!("Starting web server at http://{}:{}/", web_server_ip, web_server_port);
+
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind((web_server_ip, web_server_port))?
+        .run()
+        .await
 }
